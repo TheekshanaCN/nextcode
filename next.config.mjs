36cfee54b53c92a434c1f4401f14d729aabeec2b
Theekshana <167788494+TheekshanaCN@users.noreply.github.com>
2025-01-26 18:42:00 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,38 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-      domains: ['aceternity.com', 'images.unsplash.com', 'assets.aceternity.com', 'ishankashamal.tech'],
-    },
-  }
+  images: {
+    domains: [
+      'aceternity.com',
+      'images.unsplash.com',
+      'assets.aceternity.com',
+    ],
+  },
+  async redirects() {
+    return [
+      {
+        source: "/",
+        has: [
+          {
+            type: "host",
+            value: "nextsoftgroup.com",
+          },
+        ],
+        destination: "https://www.nextsoftgroup.com/",
+        permanent: true,
+      },
+      {
+        source: "/",
+        has: [
+          {
+            type: "host",
+            value: "www.nextsoftgroup.com",
+          },
+        ],
+        destination: "https://www.nextsoftgroup.com/",
+        permanent: true,
+      },
+    ];
+  },
+};
 
 export default nextConfig;
